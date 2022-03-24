e88031947bdd6b23596047e97ce923aa79e4b87f
LewT97 <102267697+LewT97@users.noreply.github.com>
2022-03-24 23:51:34 +0000

    Add files via upload

---
+++ burger.js
@@ -0,0 +1,5 @@
+document.body.style.background =  //sets background colour through javascript
+
+document.getElementsByClassName('mobile')[0].style.backgroundColor = "rgb(239, 225, 204)"; //gets element using class name to change the background colour of a specific division
+
+document.getElementsByClassName('footer')[0].style.backgroundColor = "rgb(80, 35, 20)"; //same as above for footer instead of mobile.
